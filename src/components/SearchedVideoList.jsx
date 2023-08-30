59d4d0bd60e2817e66ccf923c9901abaa1d170bb
Ayush Singh <asinghrajput542@gmail.com>
2023-08-30 13:33:01 +0530

    Bug fixed

--- src/components/SearchedVideoList.jsx
+++ src/components/SearchedVideoList.jsx
@@ -1,37 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import { CATAGORY_FILTER_PREFIC, VIDEO_LIST_URL } from '../utils/constant';
-import SearchVideoCard from './SearchVideoCard';
-import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { CATAGORY_FILTER_PREFIC, VIDEO_LIST_URL } from "../utils/constant";
+import SearchVideoCard from "./SearchVideoCard";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const SearchedVideoList = () => {
-    const pathParam=useParams();
-    const category=pathParam?.search
-    const [searchVideosList, setSearchVideosList]=useState([]);
-    useEffect(()=>{
-        getVideoList()
-    },[category])
-    const theme=useSelector(store=>store.theme.mode);
+  const pathParam = useParams();
+  const category = pathParam?.search;
+  const [searchVideosList, setSearchVideosList] = useState([]);
+  useEffect(() => {
+    getVideoList();
+  }, [category]);
+  const theme = useSelector((store) => store.theme.mode);
 
-useEffect(()=>{
-  if(theme==="dark"){
-    document.documentElement.classList.add("dark")
-  }else{
-    document.documentElement.classList.remove("dark")
-  }
-},[theme])
-
-    const getVideoList=async ()=>{
-        const videoData=await fetch(VIDEO_LIST_URL + CATAGORY_FILTER_PREFIC+category);
-        const videoJson= await videoData.json();
-        setSearchVideosList(videoJson.items);
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
     }
-    return (searchVideosList.length>0) && (
-    <div className='dark:bg-black dark:text-white h-[100vh]'>
-      {searchVideosList.map(video =>(<Link to={"/watch?v="+video?.id?.videoId} key={video?.id?.videoId} > <SearchVideoCard info={video}/> </Link>))};
-    </div>
-  )
-}
+  }, [theme]);
 
-export default SearchedVideoList
+  const getVideoList = async () => {
+    const videoData = await fetch(
+      VIDEO_LIST_URL + CATAGORY_FILTER_PREFIC + category
+    );
+    const videoJson = await videoData.json();
+    setSearchVideosList(videoJson.items);
+  };
+  return (
+    searchVideosList.length > 0 && (
+      <div className="dark:bg-black dark:text-white h-[100%]">
+        {searchVideosList.map((video) => (
+          <Link to={"/watch?v=" + video?.id?.videoId} key={video?.id?.videoId}>
+            {" "}
+            <SearchVideoCard info={video} />{" "}
+          </Link>
+        ))}
+        ;
+      </div>
+    )
+  );
+};
+
+export default SearchedVideoList;
